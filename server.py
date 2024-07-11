ebc8f26ed97e93c483f98416408e60606d641d2b
Ian Grzembski <iangrz2003@gmail.com>
2024-07-11 14:20:42 -0400

    Added type annotations

--- server.py
+++ server.py
@@ -6,13 +6,14 @@ import pickle
 import pyarrow.parquet as pq
 from flask import Flask, jsonify
 from sklearn.metrics import accuracy_score
+from sklearn.tree import DecisionTreeClassifier
 
 app = Flask(__name__)
 MODEL_FILENAME = "trained_model.pkl"
 with open(MODEL_FILENAME, 'rb') as file:
-    model = pickle.load(file)
+    model : DecisionTreeClassifier = pickle.load(file)
 
-def test_model(model, df):
+def test_model(model: DecisionTreeClassifier, df: pd.DataFrame):
     X_test = pd.get_dummies(df[['homeworld','unit_type']])
     y_test=df['empire_or_resistance']
     predictions =  model.predict(X_test)
@@ -23,7 +24,7 @@ def say_hello():
 
 def main():
     DATA_FILENAME = "troop_movements10m.parquet"
-    df = pq.read_pandas(DATA_FILENAME)
+    df : pd.DataFrame = pq.read_pandas(DATA_FILENAME)
     test_model(model, df)
     app.run()
 
