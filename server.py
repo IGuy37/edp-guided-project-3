03b7914cc4f59b4d945fc5c9b32b89cf58e7e8c8
Ian Grzembski <iangrz2003@gmail.com>
2024-07-11 13:51:16 -0400

    Started server.py

---
+++ server.py
@@ -0,0 +1,20 @@
+import pandas as pd 
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+import pickle
+import pyarrow.parquet as pq
+
+def main():
+    MODEL_FILENAME = "trained_model.pkl"
+    DATA_FILENAME = "troop_movements10m.parquet"
+    model = pickle.load(MODEL_FILENAME)
+    df = pq.read_pandas(DATA_FILENAME)
+    X_test = pd.get_dummies(df[['homeworld','unit_type']])
+    y_test=df['empire_or_resistance']
+    model.predict()
+
+
+
+if(__name__ == "__main__"):
+    main()
