ea4e792d7bb335717758de3689809c8da3226d71
Ian Grzembski <iangrz2003@gmail.com>
2024-07-11 13:58:36 -0400

    Refactored functions to put model and dataframe in main() function

--- server.py
+++ server.py
@@ -7,21 +7,22 @@ import pyarrow.parquet as pq
 from flask import Flask
 from sklearn.metrics import accuracy_score
 
-def test_model():
-    MODEL_FILENAME = "trained_model.pkl"
-    DATA_FILENAME = "troop_movements10m.parquet"
-    model = pickle.load(MODEL_FILENAME)
-    df = pq.read_pandas(DATA_FILENAME)
+
+def test_model(model, df):
     X_test = pd.get_dummies(df[['homeworld','unit_type']])
     y_test=df['empire_or_resistance']
     predictions =  model.predict(X_test)
 
-def serve():
+def serve(model, df):
     pass
 
 def main():
-    test_model()
-    serve()
+    MODEL_FILENAME = "trained_model.pkl"
+    DATA_FILENAME = "troop_movements10m.parquet"
+    model = pickle.load(MODEL_FILENAME)
+    df = pq.read_pandas(DATA_FILENAME)
+    test_model(model, df)
+    serve(model, df)
 
-if(__name__ == "__main__"):
+if(__name__ == "__main__"):  
     main()
