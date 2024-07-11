69b0b4a49c1b32638b94b889044ce8475ba2ebb1
Ian Grzembski <iangrz2003@gmail.com>
2024-07-11 14:34:06 -0400

    removed unnecessary imports

--- server.py
+++ server.py
@@ -1,24 +1,13 @@
 import pandas as pd 
-import numpy as np
-import seaborn as sns
-import matplotlib.pyplot as plt
 import pickle
 import pyarrow.parquet as pq
 from flask import Flask, jsonify
-from sklearn.metrics import accuracy_score
 from sklearn.tree import DecisionTreeClassifier
 
 app = Flask(__name__)
 MODEL_FILENAME = "trained_model.pkl"
 with open(MODEL_FILENAME, 'rb') as file:
     model : DecisionTreeClassifier = pickle.load(file)
-
-def test_model(model: DecisionTreeClassifier, df: pd.DataFrame):
-    X_test = pd.get_dummies(df[['homeworld','unit_type']])
-    y_test=df['empire_or_resistance']
-    predictions =  model.predict(X_test)
-    score = accuracy_score(y_test, predictions)
-    return score
 
 @app.route('/')
 def say_hello():
@@ -29,7 +18,9 @@ def main():
     DATA_FILENAME = "troop_movements10m.parquet"
     df : pd.DataFrame = pq.read_pandas(DATA_FILENAME)
     print("Done!")
-    test_model(model, df)
+    X_test = pd.get_dummies(df[['homeworld','unit_type']])
+    predictions =  model.predict(X_test)
+    df['predictions'] = predictions
     app.run()
 
 if(__name__ == "__main__"):  
